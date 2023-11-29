411815f779df956f65c6abc540a1d71e7a1af587
claratesmon <claraperezmontes@gmail.com>
2023-11-29 19:25:26 +0100

    models

---
+++ models/Testimony.Model.js
@@ -0,0 +1,27 @@
+
+const { Schema, model } = require("mongoose");
+
+const testimonySchema = new Schema(
+   
+    {
+        text: {
+            type: String,
+            required: true
+        },
+        rating: {
+            type: Number,
+            min:1,
+            max: 5,
+            required: true
+        },
+        creator: {
+            type: [{type: Schema.Types.ObjectId, ref: "testimony"}],
+            required: true
+        }
+
+
+    }
+);
+
+const Testimony = model("Testimony", testimonySchema)
+module.exports = Testimony;
--- models/User.model.js
+++ models/User.model.js
@@ -1,5 +1,10 @@
 const { Schema, model } = require("mongoose");
 
+const validateEmail = (v) => {
+  return /[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}/i.test(v);
+  message: props => `${props.value} is not a valid email address!`
+}
+
 // TODO: Please make sure you edit the User model to whatever makes sense in this case
 const userSchema = new Schema(
   {
@@ -9,15 +14,36 @@ const userSchema = new Schema(
       unique: true,
       lowercase: true,
       trim: true,
+      validate: [validateEmail, "Please use a valid email address"],
+      match: [/[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}/i.test(v), "Please use a valid email address"]
     },
     password: {
       type: String,
       required: [true, "Password is required."],
     },
-    name: {
+    username: {
       type: String,
-      required: [true, "Name is required."],
+      required: [true, "Username is required."],
     },
+    location: {
+      type: String,
+      required: true
+    },
+    profilePicture: {
+      type: String,
+      default: "default.png",
+    },
+    skills: {
+      type: String,
+      enum: ["Languages", "Strength", "Electronics", "Hardware"]
+    },
+    tokens: {
+      type: Number
+    },
+    testimonies: {
+      type: [{ type: Schema.Types.ObjectId, ref: "Testimony" }]
+    }
+
   },
   {
     // this second object adds extra properties: `createdAt` and `updatedAt`
