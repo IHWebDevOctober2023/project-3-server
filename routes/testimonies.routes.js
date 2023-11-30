476ce04032d556258e1a1ec49326e5c2293dbbf3
Martin Palkov <martin.palkov@gmail.com>
2023-11-30 19:56:13 +0100

    route GET /testimonies/landing responds correctly

--- routes/testimonies.routes.js
+++ routes/testimonies.routes.js
@@ -2,21 +2,20 @@ const express = require("express");
 const router = express.Router();
 //
 const mongoose = require("mongoose");
-const Testimony = require("../models/Testimony.model");
+const Testimony = require("../models/Testimony.Model");
 
 // /testimonies/landing
 router.get("/landing", (req, res, next) => {
-    Testimony.find().slice(4, 0)
-        .then((allTestimonies) => res.json(allTestimonies))
+    Testimony.find()
+        .then((allTestimonies) => res.send(allTestimonies.slice(0, 4)))
+        .catch((err) => console.error(err));
+        //.then((allTestimonies) => res.json(allTestimonies))
 });
 
+// CONFIRM THE BELLOW ROUTE CODE IS CORRECT 
 router.get("/createtestimony", (req, res, next) => {
-    Testimony.find()
-    .then((alltestimonies) => {
-        console.log("Alltestimonies: ", alltestimonies);
-    
-    })/* slice(4, 0)
-        .then((allTestimonies) => res.json(allTestimonies)) */
+    Testimony.find().slice(4, 0)
+        .then((allTestimonies) => res.json(allTestimonies))
 });
 
 router.post("/createtestimony", (req, res, next) => {
