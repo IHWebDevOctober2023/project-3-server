d803dd37bc4fdffef1c9dfef10ff9979168d0503
Victor <victor.castanos@gmail.com>
2023-12-05 10:03:17 +0100

    new trask routes

--- routes/task.routes.js
+++ routes/task.routes.js
@@ -34,34 +34,60 @@ router.post('/task', (req, res, next) => {
       // Send an error response if there's an issue creating the task
     });
 });
+///////////////////// MANAGING FAMILY PERFORMANCE ////////////////////
+// CREATING a route to calculate TASKS by Family
+router.get("/tasks/:_id/tasksByFamily", (req, res, next) => {
+  const familyId = req.params._id
+  console.log("hello", familyId);
+  const response = {
+    tasksByFamily: 0,
+    tasksDoneByFamily: 0,
+    tasksPendingByFamily: 0,
+    tasksDoneByUser: [],
+    tasksPendingByUser: [],
+    kpiByFamily: 0,
+    kpiByUser: 0
+  }
 
+  Task.find({ taskFamily: familyId })
+    .then((tasksbyFamily) => {
+      response.tasksByFamily = tasksbyFamily.length;
+      response.tasksDoneByFamily = tasksbyFamily.filter((eachTask) =>{
+        return eachTask.taskIsDone === true;
+      }).length
+      response.tasksPendingByFamily = tasksbyFamily.filter((eachTask) =>{
+        return eachTask.taskIsDone === false;
+      }).length
+      res.json(response)
+    })
+    .catch((error) => console.log(error))
+})
+
+
+// GET task by Day and Family
 router.get("/tasks/:_id/:dayName", (req, res, next) => {
   const dayName = req.params.dayName
   const currentDate = new Date();
   console.log(currentDate);
- // const dayOfWeek = currentDate.getDay();
+  // const dayOfWeek = currentDate.getDay();
   //const daysOfWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
   //const dayName = daysOfWeek[dayOfWeek];
   const familyId = req.params._id
-  Task.find({ taskFamily: familyId, taskWeekDay: dayName})
+  Task.find({ taskFamily: familyId, taskWeekDay: dayName })
     .then((tasks) => {
       res.json(tasks)
     })
     .catch((error) => console.log(error))
 })
 
-///////////////////// MANAGING FAMILY PERFORMANCE ////////////////////
-// CREATING a route to calculate TASKS by Family
- router.get("/tasks/:_id/:tasksByFamily", (req, res, next) => {
-  const familyId = req.params._id
-  Task.find({ taskFamily: familyId})
-    .then((TasksbyFamily) => {
-      res.json(TasksbyFamily)
-    })
-    .catch((error) => console.log(error))
-})
-// CREATING a route to calculate all the Tasks DONE by Family
-router.get("/tasks/:_id/:tasksDoneByFamily", (req, res, next) => {
+
+
+module.exports = router;
+
+
+
+/* // CREATING a route to calculate all the Tasks DONE by Family
+router.get("/tasks/:_id/tasksDoneByFamily", (req, res, next) => {
   const familyId = req.params._id
   Task.find({ taskFamily: familyId, taskIsDone: true })
     .then((TasksDonebyFamily) => {
@@ -72,7 +98,7 @@ router.get("/tasks/:_id/:tasksDoneByFamily", (req, res, next) => {
 // CREATING a route to calculate all the Tasks PENDING by Family
 router.get("/tasks/:_id/:tasksPendingByFamily", (req, res, next) => {
   const familyId = req.params._id
-  Task.find({ taskFamily: familyId, taskIsDone: true })
+  Task.find({ taskFamily: familyId, taskIsDone: false })
     .then((TasksPendingbyFamily) => {
       res.json(TasksPendingbyFamily)
     })
@@ -98,8 +124,4 @@ router.get("/tasks/:_id/:tasksPendingByUserDone", (req, res, next) => {
     })
     .catch((error) => console.log(error))
 })
-
-
-
-
-module.exports = router;
+ */
